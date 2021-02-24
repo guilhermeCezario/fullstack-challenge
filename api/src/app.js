22faf55ed1c985ebb3a87ea5adce31f140af7699
Guilherme Cezário <cezariogui@outlook.com>
2021-02-24 20:34:31 -0300

    added struture basic for nodejs

---
+++ api/src/app.js
@@ -0,0 +1,16 @@
+import 'dotenv/config';
+
+import express from 'express';
+import cors from 'cors';
+import { errors } from 'celebrate';
+import routes from './routes';
+
+import './database';
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+app.use(errors());
+
+export default app;
